a0980b89b506ee8097cf1f5904edf8615f612529
Kirill Lisov <74524495+KirVelikiyy@users.noreply.github.com>
2023-02-25 21:13:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-my_list = [1, 3, 5, 7, 9, 11, 23, 59, 98]
-def binary_search(list,item):
-    low = 0
-    high = len(list)-1
-    while low <= high:
-        mid = int((low + high)/2)
-        quess = list[mid]
-        if quess == item:
-            print("Значение найдено")
-            return mid
-        if quess > item:
-            print("Переходим в первую половину")
-            high = mid - 1
-        if quess < item:
-            print("go to right")
-            low = mid + 1
-    return "В списке нет такого числа"
-
-
-
-
-print(binary_search(my_list,23))
-
-
 def binary_search_2(arr, x):
     """
     Функция для бинарного поиска элемента x в отсортированном массиве arr
@@ -37,5 +13,4 @@ def binary_search_2(arr, x):
         else:
             return mid
     return -1
-
 
