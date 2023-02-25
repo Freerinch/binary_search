6dd6fc826eb10ebdd9d26da53061a3995d7e455c
Kirill Lisov <74524495+KirVelikiyy@users.noreply.github.com>
2023-02-25 20:59:45 +0300

    Update main.py
    add work binary search function, for example

--- main.py
+++ main.py
@@ -22,3 +22,20 @@ def binary_search(list,item):
 print(binary_search(my_list,23))
 
 
+def binary_search_2(arr, x):
+    """
+    Функция для бинарного поиска элемента x в отсортированном массиве arr
+    """
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+
